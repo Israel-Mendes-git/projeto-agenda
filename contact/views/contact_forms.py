883c5dcd7ccfdbef782f934eb3abf14791fe06bc
Israel-Mendes-git <Israelmendesmzs@gmail.com>
2025-05-07 20:22:16 -0300

    muitas mudanças
    novas adições ao projeto, muita coisa pra documentar

---
+++ contact/views/contact_forms.py
@@ -0,0 +1,32 @@
+from typing import Any, Dict
+ 
+from django import forms
+from django.core.exceptions import ValidationError
+from django.core.paginator import Paginator
+from django.db.models import Q
+from django.shortcuts import get_object_or_404, redirect, render
+ 
+from contact.forms import ContactForm
+from contact.models import Contact
+
+def create(request):
+     if request.method == 'POST':
+         context = {
+             'form': ContactForm(request.POST)
+         }
+ 
+
+         return render(
+             request,
+             'contact/create.html',
+             context
+         )
+ 
+     context = {
+         'form': ContactForm()
+     }
+ 
+     return render(
+         request,
+         'contact/create.html',
+     )
